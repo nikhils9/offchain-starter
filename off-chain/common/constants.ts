35c37c2a6c47a26b1efaa293e980cf9f148f2afc
nikhils9 <nikhilshetty447@gmail.com>
2023-10-11 14:24:07 +0530

    Add lucid utils Add required directories and files for off chain projects

---
+++ off-chain/common/constants.ts
@@ -0,0 +1,15 @@
+import { Network } from "lucid";
+
+export const NETWORK: Network = "Preprod";
+
+export const BLOCKFROST_URL = "https://cardano-preprod.blockfrost.io/api/v0";
+
+// Paths
+export const PROJECT_PATH = "/home/shetty/projects/cip68-nfts/";
+export const CREDENTIALS_PATH = PROJECT_PATH + "wallets/";
+export const APPLIED_VALIDATOR_PATH = PROJECT_PATH + "applied-validators/";
+
+// Asset Labels
+export const REFERENCE_TOKEN_LABEL = 100;
+export const NON_FUNGIBLE_TOKEN_LABEL = 222;
+export const FUNGIBLE_TOKEN_LABEL = 333;
